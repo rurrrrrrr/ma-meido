a7347c4b922b01598b2fa1b77529abb2a6b51a72
rurrrrrrr <sshuku241146@shukutoku.ed.jp>
2025-11-10 18:25:35 +0900

    Add gravity and bounce logic to circle movement
    Implement gravity and bouncing behavior for a circle.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let x;
+let y;
+let vy;
+let vx;
+let g;
+let r;
+
 function setup() {
     createCanvas(400, 400);
+    x=20;
+    y=0;
+    vy=20;
+    vx=100;
+    r=20;
+    g=1;
 }
 
 function draw() {
-    background(220);
+    background(0,0,0,1);
+
+    x = x + vx;
+    vy = vy + g;
+    y= y + vy;
+
+    if (y > height - r) {
+        y = height - r
+        vy= -vy* 0.9;
+
+    }
+    if (x<r) {
+        x=r;
+        vx=-vx;
+    }
+
+    if (x>width - r) {
+        x=width - r;
+        vx=-vx;
+    }
+    fill(random(0), random(0),random(255));
+    circle(x,y,r * 5);
 }
